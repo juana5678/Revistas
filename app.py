84de7def919a8e8fb560a08b387dd488f2e913af
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-04-05 16:46:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return '𝐇𝐨𝐥𝐚, 𝐃𝐞 𝐀𝐧𝐭𝐞𝐦𝐚𝐧𝐨 𝐆𝐫𝐚𝐜𝐢𝐚𝐬 𝐩𝐨𝐫 𝐌𝐚𝐧𝐭𝐞𝐫 𝐌𝐢 𝐒𝐞𝐫𝐯𝐢𝐜𝐢𝐨 𝐀𝐜𝐭𝐢𝐯𝐨 😊 𝐄𝐫𝐞𝐬 𝐍𝐮𝐞𝐬𝐭𝐫𝐚 𝐑𝐚𝐳𝐨́𝐧 𝐃𝐞 𝐒𝐞𝐫'
 
-os.system("git clone https://stvz21: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@github.com/stvz21/web-stvz oke && cd oke && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://stvz21: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@github.com/stvz21/Down-Revistas oke && cd oke && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
